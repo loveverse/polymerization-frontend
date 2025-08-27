cdb91c3b2f3078b155e864179fd3c219330cbfde
loveverse <398096830@qq.com>
2025-08-27 11:17:22 +0800

    feat: 增加 ts 类型工具类，优化代码规范

---
+++ packages/web-blog/src/store/permission.ts
@@ -0,0 +1,33 @@
+import { defineStore } from "pinia";
+import store from "./index";
+import type { RouteRecordRaw } from "vue-router";
+
+export interface PermissionState {
+  permissionCodes: Set<string>;
+  asyncRoutes: RouteRecordRaw[];
+}
+
+
+export const usePermissionStore = defineStore("permission", {
+  state: (): PermissionState => ({
+    permissionCodes: new Set<string>(),
+    asyncRoutes: [],
+  }),
+  actions: {
+    setPermissions(codes: string[]) {
+      this.permissionCodes = new Set(codes);
+    },
+    filterRoutesByUserRoutes(localRoutes: RouteRecordRaw[], userRoutes: RouteRecordRaw[]) {
+      // const filterMenu = () => {
+      //   return localRoutes.map()
+      // }
+      // return filterMenu(localRoutes, userRoutes)
+    },
+  },
+});
+
+export function usePermissionStoreHook() {
+  return usePermissionStore(store);
+}
+
+
